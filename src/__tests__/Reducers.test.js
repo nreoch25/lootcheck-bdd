3cedf49b78d9187df371e15f3502fe9c54a1da57
Nigel Reoch <nigel.reoch@cbc.ca>
2018-01-30 17:44:57 -0500

    initial commit

---
+++ src/__tests__/Reducers.test.js
@@ -0,0 +1,9 @@
+import balanceReducer from "../reducers/balance";
+import * as constants from "../actions/constants";
+
+describe("balanceReducer", () => {
+  it("sets a balance", () => {
+    const balance = 10;
+    expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance })).toEqual(balance);
+  });
+});
